1a840b2c43a85e6dc98a4bce9c1168722c9f1650
Michal Stasiak <michalstasiak@Michals-MacBook-Air.local>
2022-02-22 15:58:22 +0000

    complete app

---
+++ ConsumeWebApiApp/Models/Release.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ConsumeWebApiApp.Models
+{
+    public class Release
+    {
+        public string Name { get; set; }
+        public string Artists { get; set; }
+        public string Date { get; set; }
+        public string ImageUrl { get; set; }
+        public string Link { get; set; }
+    }
+}
